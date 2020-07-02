85294bcad6b5984be2d4bb0c2b2b97aa84e5273b
ymsk.sky <ymsk.sky.95@gmail.com>
2020-07-02 18:03:32 +0900

    RE abc171 c

--- abc171/c.py
+++ abc171/c.py
@@ -1,18 +1,12 @@
 n=int(input())
-al='abcdefghijklmnopqrstuvwxyz'
 ans=''
-while not n<1:
-    mod=n%26
-    ans+=al[mod-1]  # mod=0: -1='z'
-    n=n//26-1
+for i in range(1,99):
+    if n<=26**i:
+        n-=1
+        for j in range(i):
+            ans+=chr(ord('a')+n%26)
+            n//=26
+        break
+    else:
+        n-=26**i
 print(ans[::-1])
-
-"""
-702=zz
-702/26=27...0, 27/26=1...1
-ans=z
-n=27
-
-703=aaa
-703/26=27...1, 27/26=1...1
-"""
