a4060ae8132970489e08f7e2c9281b0df102133d
ymsk.sky <ymsk.sky.95@gmail.com>
2020-07-08 17:39:36 +0900

    algorithm しゃくとり法

---
+++ algorithm/two_pointers.py
@@ -0,0 +1,28 @@
+"""
+ABC038 - C 参考
+https://atcoder.jp/contests/abc038/tasks/abc038_c
+"""
+
+# 入力受付
+n = int(input())
+l = list(map(int, input().split())) + [-1]
+
+def sum_return(n):
+    return n*(n+1)//2
+
+ans = 0
+cnt = 0
+pre = -1
+
+for i in range(n+1):
+    # 条件外れるまでカウント
+    if l[i] > pre:
+        cnt += 1
+    else:
+        # カウントした長さ分の部分数列を追加
+        ans += sum_return(cnt)
+        # カウントリセット
+        cnt = 1
+    pre = l[i]
+
+print(ans)
