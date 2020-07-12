bd0ff716aec0dd75c3ad08770dfd08ca0d5c48ae
ymsk.sky <ymsk.sky.95@gmail.com>
2020-07-12 19:33:17 +0900

    abc088 a,b AC

---
+++ abc088/b.py
@@ -0,0 +1,3 @@
+n=int(input())
+l=sorted(list(map(int,input().split())),reverse=True)
+print(sum(l[0::2])-sum(l[1::2]))
---
+++ abc088/c.py
@@ -0,0 +1,2 @@
+cs=[list(map(int,input().split())) for _ in range(3)]
+print(cs)
