3e3acd58486cfec6c2900f0b3baa034d7d821e9e
ymsk.sky <ymsk.sky.95@gmail.com>
2020-07-13 17:29:15 +0900

    abc088 c AC

--- abc088/c.py
+++ abc088/c.py
@@ -1,2 +1,4 @@
 cs=[list(map(int,input().split())) for _ in range(3)]
-print(cs)
+a=[i-j for i,j in zip(cs[0],cs[1])]
+b=[i-j for i,j in zip(cs[1],cs[2])]
+print('Yes'if a[0]==a[1]==a[2] and b[0]==b[1]==b[2]else'No')
