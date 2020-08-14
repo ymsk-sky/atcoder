7b2d0913db51faee4755fad281351470156c6358
ymsk.sky <ymsk.sky.95@gmail.com>
2020-08-14 19:18:54 +0900

    abc056 a,b,c AC

---
+++ abc056/a.py
@@ -0,0 +1,11 @@
+a,b=map(str,input().split())
+if a=='H':
+    if b=='H':
+        print('H')
+    else:
+        print('D')
+else:
+    if b=='H':
+        print('D')
+    else:
+        print('H')
