934ad3a0fe5082542ad1152fa61c17c3c772fb24
ymsk.sky <ymsk.sky.95@gmail.com>
2020-09-12 21:07:24 +0900

    abc030 c input

---
+++ abc030/c.py
@@ -0,0 +1,14 @@
+from bisect import bisect_left
+n,m=map(int,input().split())
+x,y=map(int,input().split())
+al=list(map(int,input().split()))
+bl=list(map(int,input().split()))
+k='A'
+t=0
+cnt=-1
+while 1:
+    if k=='A':
+        cnt+=1
+
+    else:
+        pass
