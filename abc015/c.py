7b8ad8b28c4544243f9ec98f86a2be0f3392d296
ymsk.sky <ymsk.sky.95@gmail.com>
2020-10-03 18:58:19 +0900

    abc015 c input example

--- abc015/c.py
+++ abc015/c.py
@@ -8,3 +8,17 @@ def dfs(q,v):
             return True
     return False
 print('Found' if dfs(0,0) else 'Nothing')
+
+"""
+3 4
+1 3 5 17
+2 4 2 3
+1 3 2 9
+
+5 3
+89 62 15
+44 36 17
+4 24 24
+25 98 99
+66 33 57
+"""
