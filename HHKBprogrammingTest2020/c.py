458f738bf534963349d72ea8375b7073e9889f98
ymsk.sky <ymsk.sky.95@gmail.com>
2020-10-11 15:49:09 +0900

    HHKBpt2020 c AC(cheated)

--- HHKBprogrammingTest2020/c.py
+++ HHKBprogrammingTest2020/c.py
@@ -1,13 +1,9 @@
 n=int(input())
 l=list(map(int,input().split()))
-m=0
-for i,p in enumerate(l):
-    if p==m:
-        s=set(l[:i+1])
-        while 1:
-            m+=1
-            if m in s:
-                pass
-            else:
-                break
-    print(m)
+t=[0]*(2*10**5+1)
+at=0
+for p in l:
+    t[p]+=1
+    while t[at]:
+        at+=1
+    print(at)
