7a16426d642e615755d16d2d477ca918219946d9
ymsk.sky <ymsk.sky.95@gmail.com>
2020-10-15 19:05:19 +0900

    abc151 c AC

--- abc151/c.py
+++ abc151/c.py
@@ -1,7 +1,20 @@
 n,m=map(int,input().split())
-ps=[input().split() for _ in range(m)]
-ps=[[int(x), y] for x,y in ps]
-wa=0
+l=[list(map(str,input().split())) for _ in range(m)]
 ac=0
-for i,(p,s) in enumerate(ps):
-    if s=='WA':
+wa=0
+d={} # i番目:WA数
+for p,s in l:
+    p=int(p)
+    if p in d:
+        pass
+    else:
+        d[p]=0
+    if s=='AC':
+        if d[p]>=0:
+            ac+=1
+            wa+=d[p]
+            d[p]=-1
+    else:
+        if d[p]>=0:
+            d[p]+=1
+print(ac,wa)
