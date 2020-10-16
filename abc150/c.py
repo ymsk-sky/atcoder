eb9096d1ee0319e7c0138c6d1bfdc26425420b4d
ymsk.sky <ymsk.sky.95@gmail.com>
2020-10-16 19:57:24 +0900

    abc150 c AC

--- abc150/c.py
+++ abc150/c.py
@@ -1,3 +1,19 @@
+from itertools import permutations
 n=int(input())
 ps=list(map(int,input().split()))
 qs=list(map(int,input().split()))
+pc=0
+qc=0
+for i,s in enumerate(permutations(range(1,n+1)),start=1):
+    xc=0
+    yc=0
+    for a,x,y in zip(s,ps,qs):
+        if a==x:
+            xc+=1
+        if a==y:
+            yc+=1
+    if xc==n:
+        pc=i
+    if yc==n:
+        qc=i
+print(abs(pc-qc))
