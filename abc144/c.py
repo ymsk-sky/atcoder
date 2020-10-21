90a3168d6701bacfa1ded698cdcba4d7d88388f1
ymsk.sky <ymsk.sky.95@gmail.com>
2020-10-21 20:05:13 +0900

    abc144 c AC

--- abc144/c.py
+++ abc144/c.py
@@ -1,5 +1,18 @@
 n=int(input())
-for a in range(n//2,0,-1):
-    if n/a==n//a:
-        break
-print(a+n//a-2)
+def md(n):
+    l,u=[],[]
+    i=1
+    while i*i<=n:
+        if n%i==0:
+            l.append(i)
+            if i!=n//i:
+                u.append(n//i)
+        i+=1
+    return l+u[::-1]
+m=md(n)
+l=len(m)
+l2=len(m)//2
+if l%2==0:
+    print(m[l2]+m[l2-1]-2)
+else:
+    print(2*(m[l2]-1))
