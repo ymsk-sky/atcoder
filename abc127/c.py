534c591bf9d24ff551752ef088e046ade8069d33
ymsk.sky <ymsk.sky.95@gmail.com>
2020-10-25 19:06:19 +0900

    abc127 c AC

--- abc127/c.py
+++ abc127/c.py
@@ -1,13 +1,9 @@
 n,m=map(int,input().split())
-f=2**n-1
+gates=[0]*(n+2)
 for _ in range(m):
     l,r=map(int,input().split())
-    b=0
-    for i in range(l-1,r):
-        b=b+(1<<i)
-    f=f&b
-a=0
-while f:
-    a+=f&1
-    f=f>>1
-print(a)
+    gates[l]+=1
+    gates[r+1]-=1
+for i in range(1,n+1):
+    gates[i]+=gates[i-1]
+print(gates[1:-1].count(m))
