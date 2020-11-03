2fbd9dd2bf732ead60da9c25d13c8de4cb1ff241
ymsk.sky <ymsk.sky.95@gmail.com>
2020-11-03 18:52:06 +0900

    abc093 c AC

--- abc093/c.py
+++ abc093/c.py
@@ -1,9 +1,15 @@
-a,b,c=map(int,input().split())
-x,y,z=sorted([a,b,c])
-
-if(y-x)%2==0:
-    e1=(z-y) + (y-x)
+a,b,c=sorted(list(map(int,input().split())),reverse=True)  # 降順
+if a==b:
+    if (a-c)%2==0:
+        print((a-c)//2)
+    else:
+        print((a-c)//2+2)
+elif b==c:
+    print(a-b)
 else:
-    e1=(z-y) + (y-x+1)
-
-(z-x)
+    x=a-b
+    c+=x
+    if (a-c)%2==0:
+        print(x+(a-c)//2)
+    else:
+        print(x+(a-c)//2+2)
