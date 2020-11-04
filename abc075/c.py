7f92ff092b337eb44974f7c7c5d2dec5db155f84
ymsk.sky <ymsk.sky.95@gmail.com>
2020-11-04 19:06:02 +0900

    abc075 c continue

--- abc075/c.py
+++ abc075/c.py
@@ -1,1 +1,16 @@
 n,m=map(int,input().split())
+all_bridges=[list(map(int,input().split())) for _ in range(m)]
+cnt=0
+visited=[0]*n
+def dfs(v):
+    visited[v]=1
+    for u in range(v):
+        if visited[u]==1:
+            continue
+        dfs(u)
+
+for i in range(m):
+    bridges=all_bridges[:i]+all_bridges[i+1:]
+    visited=[0]*n
+    dfs(0)
+print(cnt)
