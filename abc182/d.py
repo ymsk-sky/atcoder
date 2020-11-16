7f005a161fa5a6c77eb598b2e569fae04ca0f26a
ymsk.sky <ymsk.sky.95@gmail.com>
2020-11-16 19:07:06 +0900

    abc182 d AC(cheated)

--- abc182/d.py
+++ abc182/d.py
@@ -1,6 +1,17 @@
 n=int(input())
-l=list(map(int,input().split()))
-finished=sum(l)
-max_=max([sum(l[i:]) for i in range(1,n+1)])
-if finished>=0:
-    
+al=list(map(int,input().split()))
+ps=[0]*n  # 合計変化
+qs=[0]*n  # 最大座標
+p=0
+q=0
+for i,a in enumerate(al):
+    p+=a
+    q=max(q,p)
+    ps[i]=p
+    qs[i]=q
+ans=0
+x=0  # 現在位置
+for p,q in zip(ps,qs):
+    ans=max(ans,x+q)
+    x+=p
+print(ans)
