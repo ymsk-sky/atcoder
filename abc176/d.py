ee28ff048b8ea316544de97bed8a69c294968ee3
ymsk.sky <ymsk.sky.95@gmail.com>
2020-11-20 19:50:12 +0900

    abc176 d not implement

--- abc176/d.py
+++ abc176/d.py
@@ -1,4 +1,20 @@
+from collections import deque
 h,w=map(int,input().split())
 ch,cw=map(int,input().split())
 dh,dw=map(int,input().split())
-s=[[c for c in input()] for _ in range(h)]
+m=[[c for c in input()] for _ in range(h)]
+visited=[[-1]*w for _ in range(h)]
+ch,cw,dh,dw=ch-1,cw-1,dh-1,dw-1
+
+def bfs(ch,cw,cost):
+    visited[ch][cw]=cost
+    q=deque([])
+    q.append([ch,cw])
+    while q:
+        y,x=q.popleft()
+        for i,j in [[0,1],[1,0],[0,-1],[-1,0]]:
+            if m[y+i][x+j]=='.' and visited[y+i][x+j]==-1:
+                visited[y+i][x+j]=cost
+                q.append([y+i,x+j])
+            elif True:
+                pass
