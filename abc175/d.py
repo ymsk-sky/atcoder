af8ab6e7e9ed4822260cb0fb8f0bf27891b521e2
ymsk.sky <ymsk.sky.95@gmail.com>
2020-12-08 19:39:28 +0900

    abc175 d implementing

--- abc175/d.py
+++ abc175/d.py
@@ -1,21 +1,30 @@
 n,k=map(int,input().split())
-ps=list(map(int,input().split()))
-cs=list(map(int,input().split()))
-m=0#max(ans)
-md={}
-for p in ps:
-    q=p-1
-    d={}
-    s=0#score
+ps=list(map(int,input().split()))  # 順列
+cs=list(map(int,input().split()))  # マスのスコア
+ans=0
+scores=[[0]*n]*n
+for i in range(n):
     for _ in range(n):
-        if q in d:
-            break
-        else:
-            s+=cs[q]
-            d[q]=s
-        q=ps[q]-1
-    tm=max(list(d.values())[:k])
-    if tm>m:
-        m=tm
-        md=d
-print(m)
+        j=ps[i]
+    scores[i][j]
+print(scores)
+"""
+n=3
+i: 0 1 2
+j: 0 1 2
+i+j: 0 1 2, 1 2 0, 2 0 1
+
+5 2
+2 4 5 1 3
+3 4 -10 -8 8
+8
+
+5 5
+2 3 4 5 1
+-1 2 3 -3 2
+1: 2 5 2 4 3 | 5 8 5 7 6 |
+2: 3 0 2 1 3
+3: -3 -1 -2 0 3
+4: 2 1 3 6 3
+5: -1 1 4 1 3
+"""
