9ebe535a8d0297c6629cd6b42d3564f64d451870
ymsk.sky <ymsk.sky.95@gmail.com>
2020-12-11 20:36:37 +0900

    abc171 d AC(cheated)

--- abc171/d.py
+++ abc171/d.py
@@ -1,20 +1,12 @@
 n=int(input())  # alの個数
-al=list(map(int,input().split()))  # a配列(この合計を出力する)
+nums=[0]*10**5
+for i in list(map(int,input().split())):
+    nums[i-1]+=1
 q=int(input())  # 操作回数
 bcs=[list(map(int,input().split())) for _ in range(q)]  # q個の操作： bをcへ変換
-dl=dict()
-for a in al:
-    if a in dl:
-        dl[a]+=1
-    else:
-        dl[a]=1
+sum_=sum([i*v for i,v in enumerate(nums,start=1)])
 for b,c in bcs:
-    if b in dl:
-        cnt_b=dl[b]  # bの個数
-        dl[b]=0
-        if c in dl:
-            dl[c]+=cnt_b
-        else:
-            dl[c]=cnt_b
-    ans=sum([k*v for k,v in dl.items()])
-    print(ans)
+    sum_+=(c-b)*nums[b-1]
+    nums[c-1]+=nums[b-1]
+    nums[b-1]=0
+    print(sum_)
