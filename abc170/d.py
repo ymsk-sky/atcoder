798aae17505220b91ecfbb933fec94db436ee8fd
ymsk.sky <ymsk.sky.95@gmail.com>
2020-12-12 20:16:26 +0900

    abc170 d RE

--- abc170/d.py
+++ abc170/d.py
@@ -1,13 +1,22 @@
+def md(n):
+    l,u=[],[]
+    i=1
+    while i*i<=n:
+        if n%i==0:
+            l.append(i)
+            if i!=n//i:
+                u.append(n//i)
+        i+=1
+    return l+u[::-1]
+
 n=int(input())
-an=list(map(int,input().split()))
-c=0
-for i in range(n):
-    for j in range(n):
-        if i==j:
-            continue
-        if an[i]%an[j]==0:
-            j=-1
-            break
-    if j==n-1:
-        c+=1
-print(c)
+l=sorted(list(map(int,input().split())),reverse=True)
+cnt=0
+for i,a in enumerate(l[:-1]):
+    x=set(l[i+1:])
+    y=set(md(a))
+    if len(x)+len(y)==len(x|y):
+        cnt+=1
+if l[-1]!=l[-2]:
+    cnt+=1
+print(cnt)
