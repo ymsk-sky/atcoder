9d3ee4c8c92362c165288ae4c64622c602658875
ymsk.sky <ymsk.sky.95@gmail.com>
2020-12-16 13:52:29 +0900

    abc147 c AC

--- abc147/c.py
+++ abc147/c.py
@@ -1,21 +1,26 @@
 from itertools import product
-N=int(input())
-shogens=[]
-for _ in range(N):
-    A=int(input())
-    shogen=[list(map(int,input().split())) for _ in range(A)]
-    shogens.append(shogen)
+n=int(input())  # 人数
+evidences=[]
+for _ in range(n):
+    a=int(input())  # 証言数
+    # xはyである(x:人, y:1のとき正直、０のとき不親切)
+    tmp=[-1]*n
+    for _ in range(a):
+        x,y=map(int,input().split())
+        tmp[x-1]=y
+    evidences.append(tmp)
 ans=0
-for states in product((0,1),repeat=N):
+for p in product((0,1),repeat=n):
+    sum_=sum(p)  # 正直者の数
     cnt=0
-    for state,shogen in zip(states,shogens):
-        if state==1:
-            for x,y in shogen:
-                if states[x-1]!=y:
+    for i,q in enumerate(p):
+        if q==1:
+            evidence=evidences[i]
+            for x,y in enumerate(evidence):
+                if y!=-1 and p[x]!=y:
                     break
             else:
-                continue
-            cnt+=1
-    if cnt==sum(states):
-        ans=max(ans,cnt)
+                cnt+=1
+    if sum_==cnt:
+        ans=max(ans,sum_)
 print(ans)
