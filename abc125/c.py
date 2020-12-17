bbfc1ab290f015c01826a2d6d38f40b1b48e1288
ymsk.sky <ymsk.sky.95@gmail.com>
2020-12-17 17:42:15 +0900

    abc125 c TLE(not good)

--- abc125/c.py
+++ abc125/c.py
@@ -1,11 +1,16 @@
-from math import gcd  # v3.4 math=>fractions
-from functools import reduce
-def gcd_list(nums):
-    return reduce(gcd, nums)
+"""
+- ひとつの数値を置き換える -> ひとつ以外の最大公約数のなかの最大のものを求める
+- ユークリッドの互除法を使う
+"""
+from math import gcd
 n=int(input())
-l=list(map(int,input().split()))
-a=1
+al=list(map(int,input().split()))
+ans=0
 for i in range(n):
-    t=l[:i]+l[i+1:]
-    a=max(a,gcd_list(t))
-print(a)
+    tmp=al[1] if i==0 else al[0]
+    for j in range(n):
+        if j==i:
+            continue
+        tmp=gcd(tmp,al[j])
+    ans=max(ans,tmp)
+print(ans)
