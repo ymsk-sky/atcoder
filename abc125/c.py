c1e2d4842b08d77950e58d0ea3037cf819c04b87
ymsk.sky <ymsk.sky.95@gmail.com>
2020-12-17 18:03:46 +0900

    abc125 c AC(cheated)

--- abc125/c.py
+++ abc125/c.py
@@ -5,12 +5,23 @@
 from math import gcd
 n=int(input())
 al=list(map(int,input().split()))
-ans=0
+l=[0]*(n+1)
+r=[0]*(n+1)
+l[0]=al[0]
+r[-1]=al[-1]
+ans=1
+for i in range(1,n+1):
+    l[i]=gcd(l[i-1],al[i-1])
+for i in range(1,n+1):
+    r[n-i]=gcd(r[n-i+1],al[n-i])
+l=l[1:]
+r=r[:-1]
 for i in range(n):
-    tmp=al[1] if i==0 else al[0]
-    for j in range(n):
-        if j==i:
-            continue
-        tmp=gcd(tmp,al[j])
-    ans=max(ans,tmp)
+    if i==0:
+        a=r[1]
+    elif i==n-1:
+        a=l[-2]
+    else:
+        a=gcd(l[i-1],r[i+1])
+    ans=max(ans,a)
 print(ans)
