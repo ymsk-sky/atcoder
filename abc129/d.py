22f050ef6ea32611919c653b2eff859b1c618d4d
ymsk.sky <ymsk.sky.95@gmail.com>
2020-12-25 17:51:40 +0900

    abc129 d concat all process but TLE

--- abc129/d.py
+++ abc129/d.py
@@ -1,36 +1,32 @@
 h,w=map(int,input().split())
 s=[input() for _ in range(h)]
 l=[[[-1,-1,-1,-1] for _ in range(w)] for _ in range(h)]
-for i in range(h):
-    for j,k in zip(range(w),range(w-1,-1,-1)):
-        if s[i][j]=='#':
-            l[i][j][0]=0
-        elif j==0:
-            l[i][j][0]=1
+for i1,i2 in zip(range(h),range(h-1,-1,-1)):
+    for j1,j2 in zip(range(w),range(w-1,-1,-1)):
+        if s[i1][j1]=='#':
+            l[i1][j1][0]=0
+        elif j1==0:
+            l[i1][j1][0]=1
         else:
-            l[i][j][0]=l[i][j-1][0]+1
-
-        if s[i][k]=='#':
-            l[i][k][1]=0
-        elif k==w-1:
-            l[i][k][1]=1
+            l[i1][j1][0]=l[i1][j1-1][0]+1
+        if s[i1][j2]=='#':
+            l[i1][j2][1]=0
+        elif j2==w-1:
+            l[i1][j2][1]=1
         else:
-            l[i][k][1]=l[i][k+1][1]+1
-for j in range(w):
-    for i,k in zip(range(h),range(h-1,-1,-1)):
-        if s[i][j]=='#':
-            l[i][j][2]=0
-        elif i==0:
-            l[i][j][2]=1
+            l[i1][j2][1]=l[i1][j2+1][1]+1
+        if s[i1][j1]=='#':
+            l[i1][j1][2]=0
+        elif i1==0:
+            l[i1][j1][2]=1
         else:
-            l[i][j][2]=l[i-1][j][2]+1
-
-        if s[k][j]=='#':
-            l[k][j][3]=0
-        elif k==h-1:
-            l[k][j][3]=1
+            l[i1][j1][2]=l[i1-1][j1][2]+1
+        if s[i2][j1]=='#':
+            l[i2][j1][3]=0
+        elif i2==h-1:
+            l[i2][j1][3]=1
         else:
-            l[k][j][3]=l[k+1][j][3]+1
+            l[i2][j1][3]=l[i2+1][j1][3]+1
 ans=0
 for i in range(h):
     for j in range(w):
