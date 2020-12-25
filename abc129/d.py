a0ca508129ca1ea09a43d8ee23995a1099279eac
ymsk.sky <ymsk.sky.95@gmail.com>
2020-12-25 17:30:07 +0900

    abc129 d TLE(fixed)

--- abc129/d.py
+++ abc129/d.py
@@ -1,45 +1,40 @@
 h,w=map(int,input().split())
 s=[input() for _ in range(h)]
-l=[[-1]*w for _ in range(h)]
+l=[[[-1,-1,-1,-1] for _ in range(w)] for _ in range(h)]
 for i in range(h):
-    for j in range(w):
+    for j,k in zip(range(w),range(w-1,-1,-1)):
         if s[i][j]=='#':
-            l[i][j]=0
+            l[i][j][0]=0
         elif j==0:
-            l[i][j]=1
+            l[i][j][0]=1
         else:
-            l[i][j]=l[i][j-1]+1
-r=[[-1]*w for _ in range(h)]
-for i in range(h):
-    for j in range(w-1,-1,-1):
-        if s[i][j]=='#':
-            r[i][j]=0
-        elif j==w-1:
-            r[i][j]=1
+            l[i][j][0]=l[i][j-1][0]+1
+
+        if s[i][k]=='#':
+            l[i][k][1]=0
+        elif k==w-1:
+            l[i][k][1]=1
         else:
-            r[i][j]=r[i][j+1]+1
-u=[[-1]*w for _ in range(h)]
+            l[i][k][1]=l[i][k+1][1]+1
 for j in range(w):
-    for i in range(h):
+    for i,k in zip(range(h),range(h-1,-1,-1)):
         if s[i][j]=='#':
-            u[i][j]=0
+            l[i][j][2]=0
         elif i==0:
-            u[i][j]=1
+            l[i][j][2]=1
         else:
-            u[i][j]=u[i-1][j]+1
-d=[[-1]*w for _ in range(h)]
-for j in range(w):
-    for i in range(h-1,-1,-1):
-        if s[i][j]=='#':
-            d[i][j]=0
-        elif i==h-1:
-            d[i][j]=1
+            l[i][j][2]=l[i-1][j][2]+1
+
+        if s[k][j]=='#':
+            l[k][j][3]=0
+        elif k==h-1:
+            l[k][j][3]=1
         else:
-            d[i][j]=d[i+1][j]+1
+            l[k][j][3]=l[k+1][j][3]+1
 ans=0
 for i in range(h):
     for j in range(w):
-        ans=max(ans,l[i][j]+r[i][j]+u[i][j]+d[i][j]-3)
+        ans=max(ans,sum(l[i][j])-3)
 print(ans)
 """
 4 6
