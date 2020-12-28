f9b399cdebf569b22e83384e088e8c07c0d37e7e
ymsk.sky <ymsk.sky.95@gmail.com>
2020-12-28 18:44:01 +0900

    abc125 d AC(cheated)

--- abc125/d.py
+++ abc125/d.py
@@ -1,30 +1,33 @@
 """
 1<=i<=n-1のiを選択後、al[i],al[i+1]に-1をかける
 任意の回数上記を行なった後のalの合計の最大を求める
-
 5
 10 -4 -8 -11 3
 30
 """
 n=int(input())
-l=list(map(int,input().split()))
-def do(i):
-    al[i]*=-1
-    al[i+1]*=-1
-ans=-float('inf')
-for al in [l, l[::-1]]:
-    for i in range(n-1):
-        a=al[i]
-        b=al[i+1]
-        if a>0 and b>0:
-            pass
-        elif a<=0 and b<=0:
-            do(i)
-        elif a<=0 and b>0:
-            if abs(a)>=b:
-                do(i)
-        elif a>0 and b<=0:
-            if abs(b)>a:
-                do(i)
-    ans=max(ans,sum(al))
+al=list(map(int,input().split()))
+cnt=0
+for a in al:
+    if a<0:
+        cnt+=1
+    elif a==0:
+        cnt=-1  # 0が存在
+        break
+if cnt==0:  # 負の数が存在しない
+    ans=sum(al)
+elif cnt==-1 or cnt%2==0:  # 0が存在または負の数が偶数
+    ans=sum([abs(a) for a in al])
+else:  # 負の数が奇数=1つを残して他は正にできる
+    min_=min([abs(a) for a in al])
+    ans=sum([abs(a) for a in al])-2*min_
 print(ans)
+"""
+x,y(x<y)のときAx,Ayを-Ax,-Ayにできる
+x=1,y=5のときy-x=4回(i=1,2,3,4:i=x,x+1,...,y-1)操作を繰り返す
+ +10  -4  -8  -11  +3
+[-10  +4] -8  -11  +3
+ -10 [-4  +8] -11  +3
+ -10  -4 [-8  +11] +3
+ -10  -4  -8 [-11  -3]
+ """
