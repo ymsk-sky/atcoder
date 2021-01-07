c85d7c637c9df818888390839576f8a812d5992f
ymsk.sky <ymsk.sky.95@gmail.com>
2021-01-07 18:36:10 +0900

    abc114 d AC(cheated)

--- abc114/d.py
+++ abc114/d.py
@@ -2,26 +2,13 @@
 n!(=1*2*3*...*n)の約数のうち約数が75個のものはいくつか
 """
 n=int(input())
-def d(n):
-    a,b,i=0,0,1
-    while i*i<=n:
-        if n%i==0:
-            a+=1
-            if i!=n//i:
-                b+=1
-        i+=1
-    return a+b
-l=[]
-for i in range(1,n+1):
-    l.append(d(i))
-print(l)
-"""
-1: 1 [1]
-2: 2 [1,2]
-3: 2 [1,3]
-4: 3 [1,2,4]
-
-6: 4 [1,2,3,6]
-8: 4 [1,2,4,8]
-48 9 [1,2,3,4,6,8,12,24,48]
-"""
+e=[0]*(n+1)
+for i in range(2,n+1):
+    cur=i
+    for j in range(2,i+1):
+        while cur%j==0:
+            e[j]+=1
+            cur//=j
+def num(m):
+    return len(list(filter(lambda x: x>=m-1,e)))
+print(num(75)+num(25)*(num(3)-1)+num(15)*(num(5)-1)+num(5)*(num(5)-1)*(num(3)-2)//2)
