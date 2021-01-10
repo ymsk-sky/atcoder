3bf37cb12d7ca64f45cb9b5cb96a1cf59d9d776d
ymsk.sky <ymsk.sky.95@gmail.com>
2021-01-10 22:17:17 +0900

    abc188 d input

--- abc188/d.py
+++ abc188/d.py
@@ -1,10 +1,2 @@
-n,C=map(int,input().split())
-l=[list(map(int,input().split())) for _ in range(n)]
-imosu=[0]*(n+1)
-for a,b,c in l:
-    imosu[a-1]+=c
-    imosu[b]-=c
-for i,v in zip(range(1,n),imosu):
-    imosu[i]+=v
-imosu=[v if v<C else C for v in imosu][:-1]
-print(sum(imosu))
+N,C=map(int,input().split())
+l=[list(map(int,input().split())) for _ in range(N)]
