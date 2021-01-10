1378b5ea77c2aa74b1d48c658732e301d72b7c0d
ymsk.sky <ymsk.sky.95@gmail.com>
2021-01-10 21:45:51 +0900

    abc188(contest) d not good

--- abc188/d.py
+++ abc188/d.py
@@ -1,11 +1,5 @@
 n,C=map(int,input().split())
 l=[list(map(int,input().split())) for _ in range(n)]
-""""""
-F=min([a for a,_,_ in l])  # 最初
-L=max([b for _,b,_ in l])  # 最後
-print(F,'~',L,L-F,'days')
-exit()
-""""""
 imosu=[0]*(n+1)
 for a,b,c in l:
     imosu[a-1]+=c
