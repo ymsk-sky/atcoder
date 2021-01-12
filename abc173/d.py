51d100d5432b412f8358aa0579071745ddafe9d2
ymsk.sky <ymsk.sky.95@gmail.com>
2021-01-12 15:35:23 +0900

    abc173 d AC(cheated)

--- abc173/d.py
+++ abc173/d.py
@@ -1,2 +1,7 @@
+from math import ceil
 n=int(input())
-l=sorted(list(map(int,input().split())),reverse=True)
+l=sorted(list(map(int,input().split())))
+ans=0
+for i in range(1,n):
+    ans+=l[n-i//2-1]
+print(ans)
