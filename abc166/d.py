81baf02f3c7ecd6d9282c696fda4152f75b52908
ymsk.sky <ymsk.sky.95@gmail.com>
2021-01-13 14:44:35 +0900

    abc166 d AC(cheated)

--- abc166/d.py
+++ abc166/d.py
@@ -1,7 +1,6 @@
 x=int(input())
-k=len(str(x))//2
-for a in range(-10**k, 10**k, 1):
-    for b in range(-10**k, 10**k, 1):
+for a in range(-118, 120, 1):
+    for b in range(-118, 120, 1):
         if ((a**5)-(b**5))==x:
             print(a,b)
             exit()
