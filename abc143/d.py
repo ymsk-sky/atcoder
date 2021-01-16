4fd1359cbb3c42774e22cd182106159128072aaa
ymsk.sky <ymsk.sky.95@gmail.com>
2021-01-16 17:57:13 +0900

    abc143 d AC(cheated)

---
+++ abc143/d.py
@@ -0,0 +1,13 @@
+from bisect import bisect_left
+n=int(input())
+l=sorted(list(map(int,input().split())))
+cnt=0
+for i in range(n):
+    for j in range(i+1,n):
+        a=l[i]
+        b=l[j]
+        k=bisect_left(l,a+b)
+        #c=l[j+1:k]
+        tmp=k-(j+1)
+        cnt+=tmp
+print(cnt)
