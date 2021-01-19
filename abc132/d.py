a211ed060e65bd8525667d9fb5ee5dd5371ee391
ymsk.sky <ymsk.sky.95@gmail.com>
2021-01-19 19:16:30 +0900

    abc132 d AC(cheated)

--- abc132/d.py
+++ abc132/d.py
@@ -3,6 +3,13 @@ n,k=map(int,input().split())
 mod=10**9+7
 def comb(n,r):
     return f(n)//(f(n-r)*f(r))
-for i in range(1,k+1):
-    ans=(comb(n-k+1,i)%mod)*(comb(k-1,i-1)%mod)
-    print(ans%mod)
+if n==k:
+    for i in range(k):
+        print(1 if i==0 else 0)
+else:
+    for i in range(1,k+1):
+        if i-n+k>1:
+            ans=0
+        else:
+            ans=(comb(n-k+1,i)%mod)*(comb(k-1,i-1)%mod)
+        print(ans%mod)
