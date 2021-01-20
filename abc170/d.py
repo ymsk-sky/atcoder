6628b6e4e36ce6547b316ab4bd48d4e4b321fd0e
ymsk.sky <ymsk.sky.95@gmail.com>
2021-01-20 19:18:42 +0900

    abc170 d AC(cheated)

--- abc170/d.py
+++ abc170/d.py
@@ -1,22 +1,19 @@
-def md(n):
-    l,u=[],[]
-    i=1
-    while i*i<=n:
-        if n%i==0:
-            l.append(i)
-            if i!=n//i:
-                u.append(n//i)
-        i+=1
-    return l+u[::-1]
-
 n=int(input())
-l=sorted(list(map(int,input().split())),reverse=True)
-cnt=0
-for i,a in enumerate(l[:-1]):
-    x=set(l[i+1:])
-    y=set(md(a))
-    if len(x)+len(y)==len(x|y):
-        cnt+=1
-if l[-1]!=l[-2]:
-    cnt+=1
-print(cnt)
+l=list(map(int,input().split()))
+m=max(l)
+dp=[0 for _ in range(m+1)]
+for a in l:
+    i=a
+    if dp[a]!=0:
+        dp[a]=2
+    else:
+        for _ in range(m):
+            if i>m:
+                break
+            dp[i]+=1
+            i+=a
+ans=0
+for a in l:
+    if dp[a]==1:
+        ans+=1
+print(ans)
