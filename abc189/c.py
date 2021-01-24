2dbf57cec0bb9e1fae84314e43fe96ae33562510
ymsk.sky <ymsk.sky.95@gmail.com>
2021-01-24 18:35:26 +0900

    abc189 c TLE(cheated)

--- abc189/c.py
+++ abc189/c.py
@@ -1,15 +1,9 @@
 n=int(input())
 al=list(map(int,input().split()))
 ans=0
-l=0
-m=float('inf')
-for r in range(1,n+1):  # 1<=r<=n
-    part=al[l:r]
-    min_=min(part)
-    len_=r-l
-    point=min_*len_
-    ans=max(ans,point)
-    if m>min_:  # updated min_
-        m=min_
-        l+=1
+for l in range(n):
+    x=al[l]
+    for r in range(l,n):
+        x=min(x,al[r])
+        ans=max(ans,x*(r-l+1))
 print(ans)
