793173bcc29fa0c1eacf83da0ede588655db7ac1
ymsk.sky <ymsk.sky.95@gmail.com>
2021-01-24 18:08:48 +0900

    abc189 b AC(cheated)

--- abc189/b.py
+++ abc189/b.py
@@ -1,8 +1,9 @@
 n,x=map(int,input().split())
 l=[list(map(int,input().split())) for _ in range(n)]
+s=0
 for i,(v,p) in enumerate(l,start=1):
-    x-=v*p/100
-    if x<0:
+    s+=v*p
+    if s>x*100:
         print(i)
         exit()
 print(-1)
