61db4421b704941c4b7c041770762e9462d849fc
ymsk.sky <ymsk.sky.95@gmail.com>
2021-02-07 19:46:29 +0900

    abc191 c AC(cheated)

--- abc191/c.py
+++ abc191/c.py
@@ -1,3 +1,13 @@
 h,w=map(int,input().split())
-ss=[list(map(int,input().split())) for _ in range(h)]
+ss=[input() for _ in range(h)]
 ans=0
+for y in range(h-1):
+    for x in range(w-1):
+        a=ss[y][x]
+        b=ss[y][x+1]
+        c=ss[y+1][x]
+        d=ss[y+1][x+1]
+        cnt=[a,b,c,d].count('#')
+        if cnt==1 or cnt==3:
+            ans+=1
+print(ans)
