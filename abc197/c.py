3c8a3858d1ec6e2a79a15babedb9c29327d0009d
ymsk.sky <ymsk.sky.95@gmail.com>
2021-03-29 18:50:36 +0900

    abc197 c TLE

--- abc197/c.py
+++ abc197/c.py
@@ -1,5 +1,24 @@
 from itertools import product
 n=int(input())
 l=list(map(int,input().split()))
-for p in product((0,1),repeat=n-1):
-    print(p)
+ans=float('inf')
+for prd in product((0,1),repeat=n-1):
+    splited=[]
+    tmp=[]
+    for i,s in enumerate(prd+(0,)):
+        tmp.append(l[i])
+        if s==1:
+            splited.append(tmp)
+            tmp=[]
+    splited.append(tmp)
+    b=splited[0][0]
+    for v in splited[0][1:]:
+        b|=v
+    crt=b
+    for a in splited[1:]:
+        b=a[0]
+        for v in a[1:]:
+            b|=v
+        crt^=b
+    ans=min(ans,crt)
+print(ans)
