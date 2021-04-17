c5dd2e7b257d736283b72b7caf6f1b87cf006d62
ymsk.sky <ymsk.sky.95@gmail.com>
2021-04-17 17:24:57 +0900

    jsc2021(contest) c TLE(no-submit)

--- jsc2021/c.py
+++ jsc2021/c.py
@@ -2,9 +2,11 @@ from math import gcd
 a,b=map(int,input().split())
 ans=1
 for x in range(a,b):
+    l=[]
     for y in range(x+1,b+1):
-        if x>=y:
+        y2=y%x
+        if y2 in l:
             break
-        tmp=gcd(x,y)
+        tmp=gcd(y2,x)
         ans=max(ans,tmp)
 print(ans)
