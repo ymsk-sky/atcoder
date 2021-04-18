686c6b5ae545d4cdc80ca89df1bd4ce79f911e74
ymsk.sky <ymsk.sky.95@gmail.com>
2021-04-18 20:34:48 +0900

    jsc2021 c AC(cheated)

--- jsc2021/c.py
+++ jsc2021/c.py
@@ -1,12 +1,5 @@
-from math import gcd
 a,b=map(int,input().split())
-ans=1
-for x in range(a,b):
-    l=[]
-    for y in range(x+1,b+1):
-        y2=y%x
-        if y2 in l:
-            break
-        tmp=gcd(y2,x)
-        ans=max(ans,tmp)
-print(ans)
+for c in range(b,0,-1):
+    if (a+c-1)//c<b//c:
+        print(c)
+        exit()
