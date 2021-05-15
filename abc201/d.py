534f503234d2b2197f98db2fe05996be150c3b2e
ymsk.sky <ymsk.sky.95@gmail.com>
2021-05-15 22:30:31 +0900

    abc201(contest) d test

--- abc201/d.py
+++ abc201/d.py
@@ -1,2 +1,17 @@
+"""
++: 青マス(乗ると+1)
+-: 赤マス(乗ると-1)
+"""
+from collections import deque
 H,W=map(int,input().split())
 A=[input() for _ in range(H)]
+q=deque()
+q.append([0,0])
+while q:
+    x,y=q.pop()
+    x1,y1=x+1,y
+    x2,y2=x,y+1
+    if x1<=W:
+        q.append([x1,y1])
+    if y2<=H:
+        q.append([x2,y2])
