926c9ca7ef1656ca60daf73deb242d893a9f2aa4
ymsk.sky <ymsk.sky.95@gmail.com>
2021-05-30 22:26:24 +0900

    abc203(contest) d TLE

--- abc203/d.py
+++ abc203/d.py
@@ -1,10 +1,12 @@
 import numpy as np
 n,k=map(int,input().split())
 l=np.array([list(map(int,input().split())) for _ in range(n)])
+k2=k**2
 ans=float('inf')
 for i in range(n-k+1):
     for j in range(n-k+1):
-        tmp=l[i:i+k,j:j+k]
-        # 要素数が偶数の場合中央の2つの値の平均となってしまう
+        tmp=np.ravel(l[i:i+k,j:j+k])
+        if k2%2==0:  # 要素数が偶数の場合中央の2つの値の平均となってしまうため0を追加
+            tmp=np.append(0,tmp)
         ans=min(ans,np.median(tmp))
 print(int(ans))
