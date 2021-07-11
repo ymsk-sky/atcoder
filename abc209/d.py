4f2c8fbcccc5285bf855677e1ecdb13625c1375c
ymsk.sky <ymsk.sky.95@gmail.com>
2021-07-11 18:45:55 +0900

    abc209 d AC(cheated)

--- abc209/d.py
+++ abc209/d.py
@@ -1,7 +1,9 @@
 """
-BFS(幅優先探索)で街cから各街への最短距離を求める(dictリスト).
-街cから街dまでの最短距離はdict[c][d]となる.
-これを記憶していき,計算していた場合はそれを再利用.
+街1からの最短距離が偶数か奇数かで考える.
+すると偶数の街から奇数の街へまたは,奇数の街から偶数の街への移動のみとなる.
+よってスタートする街が偶数か奇数かが異なっていれば同じ街で会うことはなく,
+逆に同じであれば道路で会うことはない.
+スタート地点が偶数か奇数かを調べるのみで判定可能なため計算量はO(1*q)=O(q)となる.
 """
 from collections import deque
 n,q=map(int,input().split())
@@ -11,33 +13,18 @@ graph=[[] for _ in range(n+1)]  # graph[i]: iから繋がっている街一覧(g
 for a,b in abl:
     graph[a].append(b)
     graph[b].append(a)
-calced=[[] for _ in range(n+1)]
-for c,d in cdl:
-    if len(calced[c])!=0:
-        if calced[c][d]%2==0:
-            print('Town')
-        else:
-            print('Road')
-        continue
-    elif len(calced[d])!=0:
-        if calced[d][c]%2==0:
-            print('Town')
-        else:
-            print('Road')
-        continue
-    dist=[-1]*(n+1)
-    dist[0]=0  # 不使用
-    dist[c]=0
-    dq=deque([c])
-    while dq:
-        v=dq.popleft()
-        for i in graph[v]:
-            if dist[i]>-1:
-                continue
-            dist[i]=dist[v]+1
+dist=[-1]*(n+1)  # 街1からの距離が偶数か奇数かを記憶
+dist[0]=0  # 不使用
+dist[1]=0  # 街1は街1から0(偶数)
+dq=deque([1])
+while dq:
+    v=dq.popleft()
+    for i in graph[v]:
+        if dist[i]==-1:
+            dist[i]=1-dist[v]
             dq.append(i)
-    if dist[d]%2==0:
+for c,d in cdl:
+    if dist[c]==dist[d]:
         print('Town')
     else:
         print('Road')
-    calced[c]=dist
