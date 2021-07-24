441fb8e9e050c46e7c47fe322f0366cf97afda30
ymsk.sky <ymsk.sky.95@gmail.com>
2021-07-24 22:17:38 +0900

    abc211(contest) d input

---
+++ abc211/d.py
@@ -0,0 +1,21 @@
+from collections import deque
+n,m=map(int,input().split())
+abl=[list(map(int,input().split())) for _ in range(m)]
+mod=10**9+7
+
+graph=[[] for _ in range(n+1)]
+for a,b in abl:
+    graph[a].append(b)
+    graph[b].append(a)
+
+dist=[-1]*(n+1)
+dist[0]=0
+dist[1]=0
+dq=deque([1])
+while dq:
+    v=dq.popleft()
+    for i in graph[v]:
+        if dist[i]==-1:
+            dist[i]=dist[v]+1
+            dq.append(i)
+print(dist)
