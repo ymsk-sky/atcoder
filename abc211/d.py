c4cb66fcb3538b5b893b872f67ea9a714ea38ea0
ymsk.sky <ymsk.sky.95@gmail.com>
2021-07-25 18:33:18 +0900

    abc211 d AC(cheated)

--- abc211/d.py
+++ abc211/d.py
@@ -2,15 +2,15 @@ from collections import deque
 n,m=map(int,input().split())
 abl=[list(map(int,input().split())) for _ in range(m)]
 mod=10**9+7
-
 graph=[[] for _ in range(n+1)]
 for a,b in abl:
     graph[a].append(b)
     graph[b].append(a)
-
 dist=[-1]*(n+1)
 dist[0]=0
 dist[1]=0
+cnt=[0]*(n+1)
+cnt[1]=1
 dq=deque([1])
 while dq:
     v=dq.popleft()
@@ -18,4 +18,8 @@ while dq:
         if dist[i]==-1:
             dist[i]=dist[v]+1
             dq.append(i)
-print(dist)
+            cnt[i]=cnt[v]
+        elif dist[i]==dist[v]+1:
+            cnt[i]+=cnt[v]
+            cnt[i]%=mod
+print(cnt[n])
