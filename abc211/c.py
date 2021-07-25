2a65e13814ddb6f57af1633b67e67d69d27e330d
ymsk.sky <ymsk.sky.95@gmail.com>
2021-07-25 18:23:17 +0900

    abc211 c AC(cheated)

--- abc211/c.py
+++ abc211/c.py
@@ -1,36 +1,18 @@
-from bisect import bisect_left
 s=input()
+t='chokudai'
 mod=10**9+7
-chokudai='chokudai'
-d={c:[] for c in chokudai}
-d2={c:[] for c in chokudai}
-for i,c in enumerate(s):
-    if c in d:
-        d[c].append(i)
-        if c=='i':
-            d2[c].append(1)
+n=len(s)
+dp=[[-1]*9 for _ in range(n+1)]
+for i in range(n+1):
+    for j in range(9):
+        if j==0:
+            dp[i][j]=1
         else:
-            d2[c].append(0)
-for k1,k2 in zip(chokudai[::-1][1:],chokudai[::-1]):
-    for i,dk1 in enumerate(d[k1]):
-        bi=bisect_left(d[k2],dk1)
-        for cnt in d2[k2][bi:]:
-            d2[k1][i]+=cnt
-ans=0
-for tmp in d2['c']:
-    ans+=tmp
-    ans%=mod
-print(ans)
-
-"""
-chokudaichokudaichokudai
-45
-c: 0,8,16
-h: 1,9,17
-o: 2,10,18
-k: 3,11,19
-u: 4,12,20
-d: 5,13,21
-a: 6,14,22
-i: 7,15,23
-"""
+            if i==0:
+                dp[i][j]=0
+            else:
+                if s[i-1]==t[j-1]:
+                    dp[i][j]=(dp[i-1][j]+dp[i-1][j-1])%mod
+                else:
+                    dp[i][j]=dp[i-1][j]
+print(dp[n][8])
