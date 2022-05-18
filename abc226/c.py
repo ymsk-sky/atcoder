0717015a87cec5ae36504689eff86a06fc98a030
ymsk-sky <ymsk.sky.95@gmail.com>
2022-05-18 22:38:09 +0900

    abc 226 c RE(3)

---
+++ abc226/c.py
@@ -0,0 +1,25 @@
+N = int(input())
+Tl = []
+Kl = []
+Al = []
+for _ in range(N):
+    T, K, *A = list(map(int,input().split()))
+    Tl.append(T)
+    Kl.append(K)
+    Al.append(A)
+print(Al)
+ans = 0
+fin=set([])
+
+def f(i):
+    global ans
+    if i in fin:
+        return
+    fin.add(i)
+    ans += Tl[i]
+    for a in Al[i]:
+        f(a - 1)
+
+f(N - 1)
+
+print(ans)
