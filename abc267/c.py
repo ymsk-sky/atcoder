0e7c68bbf7d77d7df6d67c4bef54352a7fab9bd9
ymsk-sky <ymsk.sky.95@gmail.com>
2022-09-03 21:34:18 +0900

    abc267 c AC (fix)

--- abc267/c.py
+++ abc267/c.py
@@ -1,2 +1,23 @@
 n, m = map(int, input().split())
 al = list(map(int, input().split()))
+
+# 1こずつの和
+sec = 0
+# i * Bi
+ans = 0
+
+for i in range(m):
+    ans += (i + 1) * al[i]
+    sec += al[i]
+
+tmp = ans
+for i in range(m, n):
+    a = al[i]
+    b = al[i - m]
+    tmp -= sec
+    sec -= b
+    tmp += m * a
+    sec += a
+    ans = max(ans, tmp)
+
+print(ans)
