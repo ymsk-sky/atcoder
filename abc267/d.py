5a5733d1c66958cc29803c9f1777f3f6f0a7b10f
ymsk-sky <ymsk.sky.95@gmail.com>
2022-09-04 17:26:05 +0900

    abc267 d AC

--- abc267/d.py
+++ abc267/d.py
@@ -10,7 +10,13 @@ dp[1][1] = al[0]
 for i in range(1, n + 1):
     a = al[i - 1]
     for j in range(1, m + 1):
-        if j == 1:
+        if j > i:
+            # 選択不可(選択数に対して母数が不足)
+            pass
+        elif n - i < m - j:
+            # 選択不可(残り個数が不足する)
+            pass
+        elif j == 1:
             dp[i][j] = max(dp[i - 1][j], a)
         else:
             dp[i][j] = max(dp[i][j], dp[i - 1][j], dp[i - 1][j - 1] + a * j)
