0d414b42214e258e551c231ca41c9054c776b5fe
ymsk-sky <ymsk.sky.95@gmail.com>
2022-09-11 18:07:46 +0900

    abc268 d implement but WA(2)

--- abc268/d.py
+++ abc268/d.py
@@ -8,22 +8,68 @@
 7. 無ければ-1を出力
 """
 
+from bisect import bisect_left
 from itertools import permutations
+
 n, m = map(int, input().split())
 sl = [input() for _ in range(n)]
 tl = [input() for _ in range(m)]
-l = set(["c_a_b"])
-bn_min = n - 1
-bn_max = 16 - sum([len(s) for s in sl])
-for p in permutations(sl):
-    for bn in range(bn_min - 1, bn_max):
-        # bn個からn-2個選ぶ
-        bs = ["__", "_", "_"]
-        tmp_s = "".join([s + b for s, b in zip(sl, bs)])
-        l.append(tmp_s)
 
-ans = l - set(tl)
-if len(ans) == 0:
-    print(-1)
-else:
-    print(ans.pop())
+if n == 1:
+    if sl[0] in tl:
+        print(-1)
+    else:
+        print(sl[0])
+    exit()
+
+def popcount(x):
+    x = x - ((x >> 1) & 0x5555555555555555)
+    x = (x & 0x3333333333333333) + ((x >> 2) & 0x3333333333333333)
+    x = (x + (x >> 4)) & 0x0f0f0f0f0f0f0f0f
+    x = x + (x >> 8)
+    x = x + (x >> 16)
+    x = x + (x >> 32)
+    return x & 0x0000007f
+
+f_min = max(0, n - 2)
+f_max = 16 - sum([len(s) for s in sl]) - 1
+
+l = []
+for per in permutations(sl):
+    qers = []
+    for f in range(f_min, f_max + 1): # f: 候補数
+        k = 2 ** f
+        for x in range(k):
+            if popcount(x) == f_min:
+                bs = []
+                crt = "_"
+                for y in range(f):
+                    if x >> y & 1 == 1:
+                        bs.append(crt)
+                        crt = "_"
+                    else:
+                        crt += "_"
+                bs.append(crt)
+                bs.append("")
+                qers.append(bs)
+
+    for qer in qers:
+        tmp = "".join(["".join([s, t]) for s, t in zip(per, qer)])
+        l.append(tmp)
+
+l.sort()
+
+print(l)
+
+can = [True] * len(l)
+for t in tl:
+    ix = bisect_left(l, t)
+    if ix < len(l):
+        if l[ix] == t:
+            can[ix] = False
+
+for i, c in enumerate(can):
+    if c:
+        print(l[i])
+        exit()
+print(-1)
