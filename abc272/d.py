77695b21158dd0837e88e66ef695279f006ea0b3
ymsk-sky <ymsk.sky.95@gmail.com>
2022-10-09 23:48:34 +0900

    abc272 d implementing

--- abc272/d.py
+++ abc272/d.py
@@ -1,40 +1,32 @@
 from collections import deque
 
-n, m  = map(int, input().split())
+n, m = map(int, input().split())
+
+"""
+0<=x<=mに対してxの平方数を事前に計算
+"""
+hl = []
+for x in range(m + 1):
+    xh = x ** 0.5
+    if xh.is_integer():
+        hl.append([x, int(xh)])
+
 INF = float("inf")
 ans = [[INF] * (n + 1) for _ in range(n + 1)]
-ans[1][1] = 0
 
 q = deque()
 q.append([1, 1])
-
 while q:
     i, j = q.popleft()
-    for k in range(1, n + 1):
-        tm = m - (i - k) ** 2
-        if tm < 0:
-            continue
-        tmp = tm ** 0.5
-        if tmp != int(tmp):
-            continue
-        else:
-            tmp = int(tmp)
-        for l in [j + tmp, j - tmp]:
-            if 1 <= l <= n:
-                if ans[k][l] == INF:
-                    ans[k][l] = ans[i][j] + 1
-                    q.append([k, l])
+    for x, xh in hl:
+        pass
+
 
 for i in range(1, n + 1):
     tmp = []
     for j in range(1, n + 1):
-        if ans[i][j] != INF:
-            tmp.append(ans[i][j])
-        else:
+        if ans[i][j] == INF:
             tmp.append(-1)
+        else:
+            tmp.append(ans[i][j])
     print(*tmp)
-
-
-"""
-i,j と k,l について (i - k)**2 + (j - l)**2 == M となるところに移動可能
-"""
