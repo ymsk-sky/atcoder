ebc06066db70320787a1165dc79d9addf4381a3a
ymsk-sky <ymsk.sky.95@gmail.com>
2022-11-20 19:02:31 +0900

    abc278 e TLE

--- abc278/e.py
+++ abc278/e.py
@@ -1,66 +1,39 @@
 H, W, N, h, w = map(int, input().split())
 al = [list(map(int, input().split())) for _ in range(H)]
 
-all_cnt = {i: 0 for i in range(1, N + 1)}
+l = [[[0] * N for _ in range(W + 1)] for _ in range(H + 1)]
+for i in range(1, H + 1):
+    for j in range(1, W + 1):
+        a = al[i - 1][j - 1]
+        l[i][j][a - 1] += 1
+
 for i in range(H):
     for j in range(W):
-        a = al[i][j]
-        all_cnt[a] += 1
+        for k in range(N):
+            l[i + 1][j + 1][k] += l[i + 1][j][k]
 
-hide = {i: 0 for i in range(1, N + 1)}
-for i in range(h):
-    for j in range(w):
-        a = al[i][j]
-        hide[a] += 1
-
-# 左端の状態を記憶
-tmp = {i: hide[i] for i in range(1, N + 1)}
+for j in range(W):
+    for i in range(H):
+        for k in range(N):
+            l[i + 1][j + 1][k] += l[i][j + 1][k]
 
 ans = []
-
-def check():
+a = l[H][W]
+def f(i, j):
+    b = l[i - 1][j - 1]
+    c = l[i - 1][j + w - 1]
+    d = l[i + h - 1][j - 1]
+    e = l[i + h - 1][j + w - 1]
     cnt = 0
-    for i in range(1, N + 1):
-        if all_cnt[i] - hide[i] > 0:
+    for x, y, z, u, v in zip(a, b, c, d, e):
+        if x - (v - z - u + y) > 0:
             cnt += 1
     return cnt
 
-# 右にずらす
-def zura(i, j):
-    for k in range(h):
-        a = al[i + k][j]
-        hide[a] -= 1
-        a = al[i + k][j + w]
-        hide[a] += 1
-
-# 下にずらす
-def zura2(i, j):
-    for k in range(w):
-        a = al[i][j + k]
-        hide[a] -= 1
-        a = al[i + h][j + k]
-        hide[a] += 1
-
-for i in range(H - h + 1):
-    l = []
-    l.append(check())
-    for j in range(W - w):
-        zura(i, j)
-        l.append(check())
-    if i != (H - h):
-        for x in range(1, N + 1):
-            hide[x] = tmp[x]
-        zura2(i, j)
-        for x in range(1, N + 1):
-            tmp[x] = hide[x]
-    ans.append(l)
-
+for i in range(1, H - h + 2):
+    tmp = []
+    for j in range(1, W - w + 2):
+        tmp.append(f(i, j))
+    ans.append(tmp)
 for an in ans:
     print(*an)
-
-"""
-1<=H,W,N<=300
-1<=h<=H
-1<=w<=W
-1<=a<=N
-"""
