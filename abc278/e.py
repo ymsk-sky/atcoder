1fa09da69bb035ae7100bb2074e86a75e43b8f77
ymsk-sky <ymsk.sky.95@gmail.com>
2022-11-21 21:10:07 +0900

    abc278 e AC

--- abc278/e.py
+++ abc278/e.py
@@ -1,39 +1,32 @@
 H, W, N, h, w = map(int, input().split())
-al = [list(map(int, input().split())) for _ in range(H)]
-
 l = [[[0] * N for _ in range(W + 1)] for _ in range(H + 1)]
 for i in range(1, H + 1):
+    al = list(map(int, input().split()))
     for j in range(1, W + 1):
-        a = al[i - 1][j - 1]
-        l[i][j][a - 1] += 1
+        l[i][j][al[j - 1] - 1] = 1
 
+# 右方向へ累積和
 for i in range(H):
     for j in range(W):
         for k in range(N):
             l[i + 1][j + 1][k] += l[i + 1][j][k]
-
+# 下方向へ累積和
 for j in range(W):
     for i in range(H):
         for k in range(N):
             l[i + 1][j + 1][k] += l[i][j + 1][k]
 
 ans = []
-a = l[H][W]
-def f(i, j):
-    b = l[i - 1][j - 1]
-    c = l[i - 1][j + w - 1]
-    d = l[i + h - 1][j - 1]
-    e = l[i + h - 1][j + w - 1]
-    cnt = 0
-    for x, y, z, u, v in zip(a, b, c, d, e):
-        if x - (v - z - u + y) > 0:
-            cnt += 1
-    return cnt
-
 for i in range(1, H - h + 2):
     tmp = []
     for j in range(1, W - w + 2):
-        tmp.append(f(i, j))
+        cnt = 0
+        for k in range(N):
+            num = (l[i - 1][j - 1][k] - l[i - 1][j + w - 1][k]
+                   - l[i + h - 1][j - 1][k] + l[i + h - 1][j + w - 1][k])
+            if l[H][W][k] - num > 0:
+                cnt += 1
+        tmp.append(cnt)
     ans.append(tmp)
 for an in ans:
     print(*an)
