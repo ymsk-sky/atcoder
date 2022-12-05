0a4ac2580f23c4c0e5846d77f90c165fc57a8372
ymsk-sky <ymsk.sky.95@gmail.com>
2022-12-05 21:19:51 +0900

    abc280 e 期待値dp

--- abc280/e.py
+++ abc280/e.py
@@ -1,18 +1,14 @@
 n, p = map(int, input().split())
 mod = 998244353
+def inv(x):
+    return pow(x, mod - 2, mod)
 
-def inv(n):
-    return pow(n, mod - 2, mod)
+prob = p * inv(100) % mod
 
-dp = [0] * (n + 1)
-dp[n] = 1
-dp[n - 1] = 100 * inv(100 - p) % mod
+# dp[i]: 残りがiのときの回数の期待値
+dp = [0] * (n + 2)
+dp[-1] = dp[0] = 0
 
-for i in range(n - 2, -1, -1):
-    num = n - i
-    dp[i] = dp[i + 1] * num * 100 * inv(100 - p) + dp[i + 2] * (num - 1) * 100 * inv(p)
-    dp[i] %= mod
-
-print(dp[0])
-
-# print(281 * pow(100, mod - 2, mod) % mod)
+for i in range(1, n + 1):
+    dp[i] = (dp[i - 2]*prob + dp[i - 1]*(1 - prob) + 1) % mod
+print(dp[n])
