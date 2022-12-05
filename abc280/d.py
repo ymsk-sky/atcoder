d078804f46c1e99a32ce9ecbabf098853efff84a
ymsk-sky <ymsk.sky.95@gmail.com>
2022-12-05 21:19:36 +0900

    abc280 d

--- abc280/d.py
+++ abc280/d.py
@@ -1,55 +1,34 @@
 k = int(input())
 
-arr = set()
-
-def fa(k):
-    tmp = k
-    for i in range(2, int(-(-k ** 0.5 // 1)) + 1):
-        if tmp % i == 0:
-            tmp //= i
-            if not i in arr:
-                arr.add(i)
-            else:
-                while 1:
-                    i *= 2
-                    if not i in arr:
-                        break
-                arr.add(i)
-    if tmp != 1:
-        if not tmp in arr:
-            fa(tmp)
-        else:
-            arr.add(tmp)
-    if arr == []:
-        if not k in arr:
-            arr.add(k)
-
-fa(k)
-
-m = max(arr)
-print(m)
-
-def factorization(n):
+def f(n):
     arr = []
-    temp = n
-    for i in range(2, int(-(-n**0.5//1))+1):
-        if temp%i==0:
-            cnt=0
-            while temp%i==0:
-                cnt+=1
-                temp //= i
-            arr.append([i, cnt])
-
-    if temp!=1:
-        arr.append([temp, 1])
-
-    if arr==[]:
-        arr.append([n, 1])
-
+    tmp = n
+    for i in range(2, int(-(-n**0.5 // 1)) + 1):
+        if tmp%i == 0:
+            cnt = 0
+            while tmp%i == 0:
+                cnt += 1
+                tmp //= i
+            arr.append((i, cnt))
+    if tmp != 1:
+        arr.append((tmp, 1))
+    if arr == []:
+        arr.append((n, 1))
     return arr
 
-
-
-"""
-2<=k<=10**12
-"""
+ans = 0
+for p, a in f(k):
+    # p**a: pがa個
+    lim = a
+    # p ~ a*p
+    for nx in range(p, a*p + 1, p):
+        j = nx
+        while j%p == 0:
+            j //= p
+            lim -= 1
+            if lim <= 0:
+                break
+        if lim <= 0:
+            break
+    ans = max(ans, nx)
+print(ans)
