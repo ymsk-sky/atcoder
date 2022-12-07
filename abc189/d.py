8b6b82eb51860bb1d11a75aad08a6901fdf9fa2b
ymsk-sky <ymsk.sky.95@gmail.com>
2022-12-07 22:28:13 +0900

    abc189 d AC

--- abc189/d.py
+++ abc189/d.py
@@ -1,1 +1,10 @@
-n=int(input())
+n = int(input())
+sl = [input() for _ in range(n)]
+y0 = 1
+y1 = 1
+for s in sl:
+    if s == "OR":
+        y0, y1 = y0*1, y0*1 + y1*2
+    elif s == "AND":
+        y0, y1 = y0*2 + y1*1, y1*1
+print(y1)
