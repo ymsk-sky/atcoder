2b3456bd74e4d21d321556f10db3d4b707e2f19b
ymsk-sky <ymsk.sky.95@gmail.com>
2022-12-07 22:30:03 +0900

    abc204 d AC DP問題

--- abc204/d.py
+++ abc204/d.py
@@ -1,11 +1,21 @@
-import numpy as np
-n=int(input())
-tl=list(map(int,input().split()))
-s=np.sum(tl)
-dp=np.zeros(s//2+1,dtype=np.bool)
-dp[-1]=1
-for i in range(n):
-    if s//2+1-tl[i]<0:
-        continue
-    dp[:s//2+1-tl[i]]|=dp[tl[i]:]
-print(s-s//2+np.argmax(dp))
+n = int(input())
+tl = list(map(int, input().split()))
+INF = float("inf")
+# dp[i][j]: i番目まで料理が終わっており, Aの時間がjのときのBの時間
+dp = [[INF] * (n * 10**3 + 1) for _ in range(n + 1)]
+dp[0][0] = 0
+for i in range(1, n + 1):
+    t = tl[i - 1]
+    for j in range(n * 10**3 + 1):
+        # A使用
+        if j - t >= 0:
+            dp[i][j] = dp[i - 1][j - t]
+        # B使用
+        dp[i][j] = min(dp[i][j], dp[i - 1][j] + t)
+
+ans = INF
+for j in range(n * 10**3 + 1):
+    # j:Aの時間, dp[n][j]:Bの時間
+    tmp = max(j, dp[n][j])
+    ans = min(ans, tmp)
+print(ans)
