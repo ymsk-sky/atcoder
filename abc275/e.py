83bc3bfc15f79627bbcc42ad17bab01f6705c5ca
ymsk-sky <ymsk.sky.95@gmail.com>
2022-12-14 21:43:08 +0900

    abc275 e DP

--- abc275/e.py
+++ abc275/e.py
@@ -1,18 +1,21 @@
-n, m, k = map(int, input().split())
+N, M, K = map(int, input().split())
 mod = 998244353
-
-# dp[i][j]: i回目にjへこれる確率
-dp = [[0] * (n + 1) for _ in range(k + 1)]
+inv = pow(M, mod - 2, mod)
+# dp[i][j]: マスjにいてi回目にkが出た
+dp = [[0] * (N + 1) for _ in range(K + 1)]
 dp[0][0] = 1
-
-for i in range(k):
-    for j in range(n + 1):
-        for deme in range(1, m + 1):
-            if j + deme > n:
-                j2 = (j + deme) % n + 1
-                dp[i + 1][-j2] += dp[i][j]
+for i in range(1, K + 1):
+    for j in range(N):
+        for k in range(1, M + 1):
+            if j + k > N:
+                m = N - (j + k - N)
+                dp[i][m] += dp[i - 1][j] * inv
+                dp[i][m] %= mod
             else:
-                dp[i + 1][j + deme] += dp[i][j]
-
-x = sum(dp[k])
-y = dp[k][n]
+                dp[i][j + k] += dp[i - 1][j] * inv
+                dp[i][j + k] %= mod
+ans = 0
+for i in range(K + 1):  # K回以下の和
+    ans += dp[i][N]
+    ans %= mod
+print(ans % mod)
