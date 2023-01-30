6141fd8f2af619eada1d4d1664e89c9f4ea3021f
ymsk-sky <ymsk.sky.95@gmail.com>
2023-01-30 22:02:22 +0900

    abc287 c implementing

---
+++ abc287/c.py
@@ -0,0 +1,24 @@
+n, m = map(int, input().split())
+edges = [[] for _ in range(n)]
+for _ in range(m):
+    u, v = map(int, input().split())
+    u, v = u - 1, v - 1
+    edges[u].append(v)
+    edges[v].append(u)
+if n - 1 != m:
+    print("No")
+    exit()
+
+e1, e2 = -1, -1
+for i in range(n):
+    if len(edges[i]) == 1:
+        if e1 == -1:
+            e1 = i
+        elif e2 == -1:
+            e2 = i
+
+l = [0] * n
+i = e1
+while 1:
+    for j in edges[i]:
+        pass
