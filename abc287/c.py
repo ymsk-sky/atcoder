7244b5edc1db462e80d1d4697d65d0a3e8dccc25
ymsk-sky <ymsk.sky.95@gmail.com>
2023-01-31 21:41:10 +0900

    abc287 c pre-fix

--- abc287/c.py
+++ abc287/c.py
@@ -16,9 +16,18 @@ for i in range(n):
             e1 = i
         elif e2 == -1:
             e2 = i
+        else:
+            print("No")
+            exit()
 
 l = [0] * n
 i = e1
+bef = -1
 while 1:
+    if len(edges[i]) > 2:
+        print("No")
+        exit()
     for j in edges[i]:
-        pass
+        if j != bef:
+            bef = i
+            break
