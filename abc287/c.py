599058cfa697b794d820f36c1ee7d073de0152c5
ymsk-sky <ymsk.sky.95@gmail.com>
2023-02-01 20:54:17 +0900

    abc287 c AC

--- abc287/c.py
+++ abc287/c.py
@@ -1,33 +1,59 @@
+class UnionFind():
+    def __init__(self, n):
+        self.n = n
+        self.root = [-1] * n
+        self.rank = [0] * n
+
+    def find(self, x):
+        if self.root[x] < 0:
+            return x
+        else:
+            self.root[x] = self.find(self.root[x])
+            return self.root[x]
+
+    def unite(self, x, y):
+        x = self.find(x)
+        y = self.find(y)
+
+        if x == y:
+            return
+        elif self.rank[x] > self.rank[y]:
+            self.root[x] += self.root[y]
+            self.root[y] = x
+        else:
+            self.root[y] += self.root[x]
+            self.root[x] = y
+            if self.rank[x] == self.rank[y]:
+                self.rank[y] += 1
+
+    def same(self, x, y):
+        return self.find(x) == self.find(y)
+
+    def size(self, x):
+        return -self.root[self.find(x)]
+
+    def roots(self):
+        return [i for i, x in enumerate(self.root) if x < 0]
+
+    def group_size(self):
+        return len(self.roots())
+
 n, m = map(int, input().split())
+uf = UnionFind(n)
 edges = [[] for _ in range(n)]
 for _ in range(m):
     u, v = map(int, input().split())
     u, v = u - 1, v - 1
     edges[u].append(v)
     edges[v].append(u)
+    uf.unite(u, v)
 if n - 1 != m:
     print("No")
     exit()
 
-e1, e2 = -1, -1
-for i in range(n):
-    if len(edges[i]) == 1:
-        if e1 == -1:
-            e1 = i
-        elif e2 == -1:
-            e2 = i
-        else:
-            print("No")
-            exit()
+l = [len(edge) for edge in edges]
 
-l = [0] * n
-i = e1
-bef = -1
-while 1:
-    if len(edges[i]) > 2:
-        print("No")
-        exit()
-    for j in edges[i]:
-        if j != bef:
-            bef = i
-            break
+if l.count(1) == 2 and l.count(2) == n - 2 and uf.group_size() == 1:
+    print("Yes")
+else:
+    print("No")
