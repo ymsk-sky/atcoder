c0a6825ec23905ea6d0c4d7b05ded4ff39d30dcd
ymsk-sky <ymsk.sky.95@gmail.com>
2023-02-28 21:37:20 +0900

    abc291 e AC トポロジカルソート

--- abc291/e.py
+++ abc291/e.py
@@ -1,39 +1,45 @@
 from collections import deque
 
 n, m = map(int, input().split())
-edges = [set() for _ in range(n)]
-tos = [0] * n
+edges = [[] for _ in range(n)]
+in_nums = [0] * n
 for _ in range(m):
     x, y = map(int, input().split())
     x, y = x - 1, y - 1
-    edges[x].add(y)
-    tos[y] = 1
-
-if tos.count(0) != 1:
-    print("No")
-    exit()
-
-INF = float("inf")
-dist = [INF] * n
+    edges[y].append(x)
+    in_nums[x] += 1
 
 que = deque()
+tmp = 0
 for i in range(n):
-    if tos[i] == 0:
-        dist[i] = 0
-        que.append((0, i))
-        break
-while que:
-    cost, crt = que.popleft()
-    for nxt in edges[crt]:
-        if dist[nxt] <= cost - 1:
-            continue
-        dist[nxt] = cost - 1
-        que.append((cost - 1, nxt))
+    if in_nums[i] == 0:
+        que.append(i)
+        tmp += 1
 
-if INF in dist or len(set(dist)) != n:
+if tmp != 1:
     print("No")
     exit()
- 
-dist = [abs(d) + 1 for d in dist]
+
+ans = [0] * n
+num = n
+while 1:
+    crt = que.popleft()
+    tmp -= 1
+
+    ans[crt] = num
+    num -= 1
+    if num == 0:
+        break
+
+    for nxt in edges[crt]:
+        in_nums[nxt] -= 1
+        if in_nums[nxt] == 0:
+            que.append(nxt)
+            tmp += 1
+    
+    if tmp != 1:
+        print("No")
+        exit()
+
 print("Yes")
-print(*dist)
+print(*ans)
