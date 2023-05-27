631d587ff17674c0178c30039371bf45dcc6041b
ymsk-sky <ymsk.sky.95@gmail.com>
2023-05-28 00:52:50 +0900

    abc303 e AC(cheated) 次数管理してBFS

--- abc303/e.py
+++ abc303/e.py
@@ -1,47 +1,36 @@
 from collections import deque
 
 n = int(input())
-edges = [set() for _ in range(n)]
-in_nums = [0]*n  # 次数
+edges = [[] for _ in range(n)]
+in_nums = [0] * n  # 次数
 for _ in range(n - 1):
     u, v = map(int, input().split())
     u, v = u - 1, v - 1
-    edges[u].add(v)
-    edges[v].add(u)
+    edges[u].append(v)
+    edges[v].append(u)
     in_nums[u] += 1
     in_nums[v] += 1
 
-que = deque([])
-for i in range(n):
-    if in_nums[i] == 1:
-        que.append(i)
+leaf = [i for i in range(n) if in_nums[i] == 1][0]
+center = edges[leaf][0]
 
-fin = [0]*n
-ans = []
+INF = float("inf")
+dist = [INF] * n
+dist[center] = 0
+
+que = deque([center])
 while que:
     crt = que.popleft()
-    if fin[crt]:
-        continue
-    fin[crt] = 1
-    center = list(edges[crt])[0]
-    fin[center] = 1
-    cnt = 1
-    for nxt in edges[center]:
-        if nxt == crt:
+    for nxt in edges[crt]:
+        if dist[nxt] <= dist[crt] + 1:
             continue
-        cnt += 1
-        if in_nums[nxt] == 1:
-            fin[nxt] = 1
-        else:
-            in_nums[nxt] -= 1
-            edges[nxt].remove(center)
-            if in_nums[nxt] == 1:
-                fin[nxt] = 1
-                mxt = list(edges[nxt])[0]
-                que.append(mxt)
-                edges[mxt].remove(nxt)
-    ans.append(cnt)
+        dist[nxt] = dist[crt] + 1
+        que.append(nxt)
 
+ans = []
+for i in range(n):
+    if dist[i]%3 == 0:
+        ans.append(len(edges[i]))
 ans.sort()
 print(*ans)
 
