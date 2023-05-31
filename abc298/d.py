579ee35f301a3ec0bd0d6f3d71d7cd569169053e
ymsk-sky <ymsk.sky.95@gmail.com>
2023-05-31 18:13:08 +0900

    abc298 d AC(解説確認/自実装)

--- abc298/d.py
+++ abc298/d.py
@@ -1,16 +1,20 @@
+from collections import deque
+
 q = int(input())
 ql = [list(map(int, input().split())) for _ in range(q)]
 mod = 998244353
 s = 1  # 数字
-k = 1  # 桁数
+que = deque([1])  # 各桁の数値を管理
 for query in ql:
     if query[0] == 1:
         x = query[1]
         s *= 10
         s += x
-        k += 1
+        s %= mod
+        que.append(x)
     elif query[0] == 2:
-        k -= 1
-        s %= 10**k
+        x = que.popleft()
+        s -= x * pow(10, len(que), mod)
+        s %= mod
     else:
-        print(s%mod)
+        print(s)
