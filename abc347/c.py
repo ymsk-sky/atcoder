ca0cd1c8186c5cea0ae424a421698af752459d3d
ymsk-sky <ymsk.sky.95@gmail.com>
2024-04-01 21:34:24 +0900

    abc347 c 解説AC 7WA1RE 最悪

--- abc347/c.py
+++ abc347/c.py
@@ -2,6 +2,21 @@ n, a, b = map(int, input().split())
 dl = list(map(int, input().split()))
 w = a + b
 dl = [d % w for d in dl]
-l = max(dl) - min(dl) + 1
-m = (min(dl) - max(dl) + 1) % w
-print("Yes" if l <= a or m <= a else "No")
+dl = sorted(set(dl))
+m = len(dl)
+
+if m == 1:
+    print("Yes")
+    exit()
+
+for i in range(m):
+    # d1を休日の最終日, d2を休日の初日と仮定
+    # その間がb(平日)日数より多ければ
+    # d2 - d1ではないd1-d2間がすべて休日内
+    d1 = dl[i]
+    d2 = dl[(i + 1) % m]
+    if (d2 - d1) % w > b:
+        print("Yes")
+        break
+else:
+    print("No")
