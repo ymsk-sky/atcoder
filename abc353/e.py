937f2b19e1b01c459dcdbeddf9c9cfe475b9ca9b
ymsk-sky <ymsk.sky.95@gmail.com>
2024-05-11 23:40:33 +0900

    abc353 e AC 考え方一緒でDFS実装

--- abc353/e.py
+++ abc353/e.py
@@ -1,32 +1,27 @@
+import sys
+from collections import defaultdict
+sys.setrecursionlimit(10**7)
+
 n = int(input())
 sl = list(input().split())
-d = [dict(), dict()]
+d = defaultdict(list)
 for s in sl:
-    k = ord(s[0]) - 96
-    if k in d[0]:
-        d[0][k].append(s)
-    else:
-        d[0][k] = [s]
-i = 0
+    d[s[0]].append(s)
 ans = 0
-while len(d[0]) > 0 or len(d[1]) > 0:
-    for k in d[i % 2].keys():
-        lk = len(d[i % 2][k])
-        ans += lk * (lk - 1) // 2
-    i += 1
-    for k in d[(i + 1) % 2].keys():
-        for v in d[(i + 1) % 2][k]:
-            if len(v) < i + 1:
+def dfs(d, i):
+    global ans
+    for k in d:
+        l = len(d[k])
+        ans += l * (l - 1) // 2
+        d2 = defaultdict(list)
+        for s in d[k]:
+            if len(s) < i + 1:
                 continue
-            # k2 = "".join([k, v[i]])
-            k2 = k*100 + (ord(v[i]) - 96)
-            if k2 in d[i % 2]:
-                d[i % 2][k2].append(v)
-            else:
-                d[i % 2][k2] = [v]
-    d[(i + 1) % 2] = dict()
+            d2[s[i]].append(s)
+        if len(d2) > 0:
+            dfs(d2, i + 1)
+dfs(d, 1)
 print(ans)
-
 
 """
 2<=n<=3*10**5
@@ -34,6 +29,8 @@ print(ans)
 sum(|s|) <= 3*10**5
 
 11
+ab bb aaa bba baba babb aaaba aabbb a a b
+
 a
 a
 aaa
