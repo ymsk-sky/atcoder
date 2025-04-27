028469a3bd81c909d918469456e55efa4a0f034f
ymsk-sky <ymsk.sky.95@gmail.com>
2025-04-27 18:27:21 +0900

    [WIP] ahc046 残り経路復元

--- AHC/ahc046_after.py
+++ AHC/ahc046_after.py
@@ -19,7 +19,7 @@ LIMIT_TIME = 1.8  # この値[s]を超えると処理終了
 C = 1.0  # 補正値: 処理時間*Cを考慮(C>=1.0)
 INF = 1 << 60
 GOAL = 1
-WALL = 2
+BLOCK = 2
 
 def input() -> str:
     return sys.stdin.readline().strip()
@@ -40,6 +40,11 @@ def reverse_dir(d: str) -> str:
         return "R"
     return "L"
 
+def get_d(p: int, q: int) -> str:
+    if p == 0:
+        return " RL"[q]
+    return " DU"[p]
+
 def solve() -> None:
     """回答"""
     fin_time = 0.0
@@ -53,19 +58,23 @@ def solve() -> None:
     for i, j in goals:
         maze[i][j] = GOAL
 
-    def bfs(si: int, sj: int, gi: int, gj: int) -> int:
-        """(si, sj) -> (gi, gj)への最短距離"""
+    def bfs(si: int, sj: int, gi: int, gj: int) -> tuple[int, list]:
+        """(si, sj) -> (gi, gj)への最短距離
+        追加で移動を復元
+        """
         dist = [[INF] * N for _ in range(N)]
         dist[si][sj] = 0
         que = deque([(si, sj)])
         while que:
             i, j = que.popleft()
+            if i == gi and j == gj:
+                break
             # 移動M
             for p, q in ((0, 1), (0, -1), (1, 0), (-1, 0)):
                 u, v = i + p, j + q
                 if 0 > u or u >= N or 0 > v or v >= N:
                     continue
-                if maze[u][v] == WALL:
+                if maze[u][v] == BLOCK:
                     continue
                 if dist[u][v] <= dist[i][j] + 1:
                     continue
@@ -75,45 +84,57 @@ def solve() -> None:
             for p, q in ((0, 1), (0, -1), (1, 0), (-1, 0)):
                 for x in range(1, N + 1):
                     u, v = i + x * p, j + x * q
-                    if 0 > u or u >= N or 0 > v or v >= N or maze[u][v] == WALL:
+                    if 0 > u or u >= N or 0 > v or v >= N or maze[u][v] == BLOCK:
                         u, v  = i + (x - 1) * p, j + (x - 1) * q
                         if dist[u][v] > dist[i][j] + 1:
                             dist[u][v] = dist[i][j] + 1
                             que.append((u, v))
                         break
-        return dist[gi][gj]
+        return dist[gi][gj], [("M", "?")]
 
-    sl = [[] for _ in range(M)]  # 移動後に壁を設置する記録
+    results = []
+    # 移動前に壁を仮設置して、改善された場合のみ採用していく
     for idx in range(M - 1):
         crt_time = time()
         if check_stopping(crt_time, fin_time):
             break
-        # 目的地に到着時に壁を仮設置して、改善された場合のみ採用
-        score_max = 0  # 何もしない場合の現状のスコア
-        for jdx in range(M - 1):
+
+        score_crt = 0  # 何もしない場合の現状のスコア
+        for jdx in range(idx, M - 1):
             si, sj = goals[jdx]
             gi, gj = goals[jdx + 1]
-            score_max += bfs(si, sj, gi, gj)
+            tmp, _ = bfs(si, sj, gi, gj)
+            score_crt += tmp
 
         for p, q in ((0, 1), (0, -1), (1, 0), (-1, 0)):
-            score_tmp = 0
-            for jdx in range(M - 1):
-                si, sj = goals[jdx]
+            si, sj = goals[idx]
+            u, v = si + p, sj + q
+            if 0 > u or u >= N or 0 > v or v >= N:
+                continue
+            if maze[u][v] == BLOCK or maze[u][v] == GOAL:
+                continue
+            maze[u][v] = BLOCK
+            score_tmp = 1  # 変更A
+            for jdx in range(idx, M - 1):
                 gi, gj = goals[jdx + 1]
-                score_tmp += bfs(si, sj, gi, gj)
-                if jdx + 1 == idx:
-                    u, v = gi + p, gj + q
-                    if 0 > u or u >= N or 0 > v or v >= N:
-                        continue
-                    if maze[u][v] == WALL or maze[u][v] == GOAL:
-                        continue
-                    score_tmp += 1  # 変更A
-                    maze[u][v] = WALL
+                tmp, l_tmp = bfs(si, sj, gi, gj)
+                score_tmp += tmp
+                if jdx == idx:
+                    l = l_tmp[:]
+
+            if score_tmp < score_crt:
+                # 採用
+                results.append(("A", get_d(p, q)))
+                results.extend(l)
+            else:
+                # 不採用
+                maze[u][v] = 0  # 元に戻す
 
         fin_time = time()
 
     # 回答
-    print("fin", idx)
+    for res in results:
+        print(*res)
 
 
 if __name__ == "__main__":
